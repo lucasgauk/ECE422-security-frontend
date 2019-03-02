7d242c938c9ee95a25c8ce8c34610a3ba842fcf6
lgauk <lucas.gauk@gmail.com>
2019-03-01 17:03:41 -0700

    Encrypt files as they are sent to the server, decrypt on the way out. Need to use something better as a key.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'localhost:8000'
+  apiUrl: 'localhost:8000',
+  encryptCode: 'MyEncryptionCode!ThisShouldChangeEveryTimeYouBuildForANewClient'
 };
